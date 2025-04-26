f91b32d56118f73438799ec2e479f6c23f8e6778
DoubleF3lix <48461905+DoubleF3lix@users.noreply.github.com>
2025-04-26 01:35:20 -0500

    Fixed missing interface implementations

--- HubPartials/ItemHub.cs
+++ HubPartials/ItemHub.cs
@@ -4,7 +4,27 @@ using Microsoft.AspNetCore.SignalR;
 namespace liveorlive_server.HubPartials
 {
     public partial class LiveOrLiveHub : Hub<IHubServerResponse>, IItemRequest {
-        public async Task UseAdrenalineItem() {
+        public async Task UseReverseTurnOrderItem() {
+
+        }
+
+        public async Task UseRackChamberItem() {
+            
+        }
+
+        public async Task UseExtraLifeItem(string target) {
+            
+        }
+
+        public async Task UsePickpocketItem(string target, Item item, string? itemTarget) {
+            
+        }
+
+        public async Task UseLifeGambleItem() {
+            
+        }
+
+        public async Task UseInvertItem() {
             
         }
 
@@ -16,28 +36,12 @@ namespace liveorlive_server.HubPartials
             
         }
 
-        public async Task UseExtraLifeItem(string target) {
-            
-        }
-
-        public async Task UseInvertItem() {
-            
-        }
-
-        public async Task UsePickpocketItem(string target, Item item, string? itemTarget) {
-            
-        }
-
-        public async Task UseRackChamberItem() {
-            
-        }
-
-        public async Task UseReverseTurnOrderItem() {
-            
-        }
-
         public async Task UseSkipItem(string target) {
             
+        }
+
+        public async Task UseRicochetItem(string target) {
+
         }
     }
 }
